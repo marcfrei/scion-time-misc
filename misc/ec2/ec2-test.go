aba009221a324719070561f4904016ea97e4b344
Marc Frei <marc.frei@gmail.com>
2023-08-29 21:04:53 +0200

    Work on test setup

--- misc/ec2/ec2-test.go
+++ misc/ec2/ec2-test.go
@@ -10,6 +10,7 @@ import (
 	"sync"
 	"time"
 
+	"github.com/pkg/sftp"
 	"golang.org/x/crypto/ssh"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
@@ -22,7 +23,7 @@ const (
 	usage = "<usage>"
 
 	ec2ImageId                       = "ami-0b2bca38b9ad1d86b"
-	ec2InstanceCount                 = 5
+	ec2InstanceCount                 = 1
 	ec2InstanceKeyName               = "ddos-testnet"
 	ec2InstanceName                  = "scion-time-test"
 	ec2InstancePrivateIpAddressCount = 3
@@ -32,6 +33,79 @@ const (
 	ec2Region                        = "eu-central-1"
 	ec2SecurityGroupId               = "sg-0faa998b9f96f3ab2"
 	ec2SubnetId                      = "subnet-0ff6cc969e67bd0ab"
+)
+
+var (
+	installGoCommands = []string{
+		"curl -LO https://go.dev/dl/go1.17.13.linux-arm64.tar.gz",
+		"echo \"914daad3f011cc2014dea799bb7490442677e4ad6de0b2ac3ded6cee7e3f493d go1.17.13.linux-arm64.tar.gz\" | sha256sum -c",
+		"sudo tar -C /usr/local -xzf go1.17.13.linux-arm64.tar.gz",
+		"sudo mv /usr/local/go /usr/local/go1.17.13",
+		"rm go1.17.13.linux-arm64.tar.gz",
+		"curl -LO https://golang.org/dl/go1.19.12.linux-arm64.tar.gz",
+		"echo \"18da7cf1ae5341e6ee120948221aff96df9145ce70f429276514ca7c67c929b1 go1.19.12.linux-arm64.tar.gz\" | sha256sum -c",
+		"sudo tar -C /usr/local -xzf go1.19.12.linux-arm64.tar.gz",
+		"sudo mv /usr/local/go /usr/local/go1.19.12",
+		"rm go1.19.12.linux-arm64.tar.gz",
+	}
+	installSCIONCommands = []string{
+		"sudo yum update",
+		"sudo yum install -y git",
+		"git clone https://github.com/scionproto/scion.git",
+		"cd /home/ec2-user/scion && /usr/local/go1.19.12/bin/go build -o ./bin/ ./control/cmd/control",
+		"cd /home/ec2-user/scion && /usr/local/go1.19.12/bin/go build -o ./bin/ ./daemon/cmd/daemon",
+		"cd /home/ec2-user/scion && /usr/local/go1.19.12/bin/go build -o ./bin/ ./dispatcher/cmd/dispatcher",
+		"cd /home/ec2-user/scion && /usr/local/go1.19.12/bin/go build -o ./bin/ ./router/cmd/router",
+		"cd /home/ec2-user/scion && /usr/local/go1.19.12/bin/go build -o ./bin/ ./scion/cmd/scion",
+	}
+	installSNCCommands = []string{
+		"sudo yum update",
+		"sudo yum install -y git",
+		"git clone https://github.com/netsec-ethz/scion.git scion-snc",
+		"cd /home/ec2-user/scion-snc && git checkout br_scheduling_snc",
+		"cd /home/ec2-user/scion-snc && /usr/local/go1.17.13/bin/go build -o ./bin/ ./go/posix-router",
+		"ln -sf /home/ec2-user/scion-snc/bin/posix-router /home/ec2-user/scion/bin/router",
+	}
+	installTSCommands = []string{
+		"sudo yum update",
+		"sudo yum install -y git",
+		"git clone https://github.com/marcfrei/scion-time.git",
+		"cd /home/ec2-user/scion-time && /usr/local/go1.19.12/bin/go build timeservice.go timeservicex.go",
+	}
+	installTestnetCommands = []string{
+		"mkdir /home/ec2-user/testnet",
+		"mkdir /home/ec2-user/testnet/gen",
+		"mkdir /home/ec2-user/testnet/systemd",
+		"mkdir /home/ec2-user/testnet/gen/ASff00_0_110",
+		"mkdir /home/ec2-user/testnet/gen/ASff00_0_120",
+		"mkdir /home/ec2-user/testnet/gen/ASff00_0_130",
+	}	
+	testnetFiles = map[string]string{
+		"./testnet/gen/ASff00_0_110/br1.toml":                "/home/ec2-user/testnet/gen/ASff00_0_110/br1.toml",
+		"./testnet/gen/ASff00_0_110/cs1.toml":                "/home/ec2-user/testnet/gen/ASff00_0_110/cs1.toml",
+		"./testnet/gen/ASff00_0_110/dispatcher.toml":         "/home/ec2-user/testnet/gen/ASff00_0_110/dispatcher.toml",
+		"./testnet/gen/ASff00_0_110/sd1.toml":                "/home/ec2-user/testnet/gen/ASff00_0_110/sd1.toml",
+		"./testnet/gen/ASff00_0_110/topology.json":           "/home/ec2-user/testnet/gen/ASff00_0_110/topology.json",
+		"./testnet/gen/ASff00_0_120/br1.toml":                "/home/ec2-user/testnet/gen/ASff00_0_120/br1.toml",
+		"./testnet/gen/ASff00_0_120/cs1.toml":                "/home/ec2-user/testnet/gen/ASff00_0_120/cs1.toml",
+		"./testnet/gen/ASff00_0_120/dispatcher.toml":         "/home/ec2-user/testnet/gen/ASff00_0_120/dispatcher.toml",
+		"./testnet/gen/ASff00_0_120/sd1.toml":                "/home/ec2-user/testnet/gen/ASff00_0_120/sd1.toml",
+		"./testnet/gen/ASff00_0_120/topology.json":           "/home/ec2-user/testnet/gen/ASff00_0_120/topology.json",
+		"./testnet/gen/ASff00_0_130/br1.toml":                "/home/ec2-user/testnet/gen/ASff00_0_130/br1.toml",
+		"./testnet/gen/ASff00_0_130/cs1.toml":                "/home/ec2-user/testnet/gen/ASff00_0_130/cs1.toml",
+		"./testnet/gen/ASff00_0_130/dispatcher.toml":         "/home/ec2-user/testnet/gen/ASff00_0_130/dispatcher.toml",
+		"./testnet/gen/ASff00_0_130/sd1.toml":                "/home/ec2-user/testnet/gen/ASff00_0_130/sd1.toml",
+		"./testnet/gen/ASff00_0_130/topology.json":           "/home/ec2-user/testnet/gen/ASff00_0_130/topology.json",
+		"./testnet/systemd/scion-border-router@.service":     "/home/ec2-user/testnet/systemd/scion-border-router@.service",
+		"./testnet/systemd/scion-control-service@.service":   "/home/ec2-user/testnet/systemd/scion-control-service@.service",
+		"./testnet/systemd/scion-daemon@.service":            "/home/ec2-user/testnet/systemd/scion-daemon@.service",
+		"./testnet/systemd/scion-dispatcher@.service":        "/home/ec2-user/testnet/systemd/scion-dispatcher@.service",
+		"./testnet/systemd/scion-timeservice-client.service": "/home/ec2-user/testnet/systemd/scion-timeservice-client.service",
+		"./testnet/systemd/scion-timeservice-server.service": "/home/ec2-user/testnet/systemd/scion-timeservice-server.service",
+		"./testnet/client.toml":                              "/home/ec2-user/testnet/client.toml",
+		"./testnet/server.toml":                              "/home/ec2-user/testnet/server.toml",
+		"./testnet/topology.topo":                            "/home/ec2-user/testnet/topology.topo",
+	}
 )
 
 func newEC2Client() *ec2.Client {
@@ -71,7 +145,7 @@ func listInstances() {
 	}
 }
 
-func runCommand(client *ssh.Client, instanceId, instanceAddr, command string) {
+func runCommand(sshClient *ssh.Client, instanceId, instanceAddr, command string) {
 	for n := 0; n < 8; n++ {
 		err := os.MkdirAll("logs", 0755)
 		if err != nil {
@@ -85,7 +159,7 @@ func runCommand(client *ssh.Client, instanceId, instanceAddr, command string) {
 			return
 		}
 		defer f.Close()
-		sess, err := client.NewSession()
+		sess, err := sshClient.NewSession()
 		if err != nil {
 			log.Printf("Failed to run command on instance %s (%s): %v", instanceId, instanceAddr, err)
 			return
@@ -123,6 +197,45 @@ func runCommand(client *ssh.Client, instanceId, instanceAddr, command string) {
 	}
 }
 
+func runCommands(sshClient *ssh.Client, instanceId, instanceAddr string, commands []string) {
+	for _, command := range commands {
+		runCommand(sshClient, instanceId, instanceAddr, command)
+	}
+}
+
+func uploadData(client *ssh.Client, instanceId, instanceAddr string, srcRd io.Reader, dstPath string) {
+	sftp, err := sftp.NewClient(client)
+	if err != nil {
+		log.Printf("Failed to upload file to instance %s (%s): %v", instanceId, instanceAddr, err)
+		return
+	}
+	defer sftp.Close()
+
+	dst, err := sftp.Create(dstPath)
+	if err != nil {
+		log.Printf("Failed to upload file to instance %s (%s): %v @1 %s", instanceId, instanceAddr, err, dstPath)
+		return
+	}
+	defer dst.Close()
+
+	_, err = dst.ReadFrom(srcRd)
+	if err != nil {
+		log.Printf("Failed to upload file to instance %s (%s): %v", instanceId, instanceAddr, err)
+		return
+	}
+}
+
+func uploadFile(client *ssh.Client, instanceId, instanceAddr, srcPath, dstPath string) {
+	src, err := os.Open(srcPath)
+	if err != nil {
+		log.Printf("Failed to upload file to instance %s (%s): %v", instanceId, instanceAddr, err)
+		return
+	}
+	defer src.Close()
+
+	uploadData(client, instanceId, instanceAddr, src, dstPath)
+}
+
 func sshIdentity(path string) ssh.AuthMethod {
 	key, err := os.ReadFile(path)
 	if err != nil {
@@ -133,6 +246,29 @@ func sshIdentity(path string) ssh.AuthMethod {
 		log.Fatalf("ParsePrivateKey (%s) failed: %v", path, err)
 	}
 	return ssh.PublicKeys(signer)
+}
+
+func uploadTestnet(sshClient *ssh.Client, instanceId, instanceAddr string) {
+	runCommands(sshClient, instanceId, instanceAddr, installTestnetCommands)
+	for src, dst := range testnetFiles {
+		uploadFile(sshClient, instanceId, instanceAddr, src, dst)
+	}
+}
+
+func installTS(sshClient *ssh.Client, instanceId, instanceAddr string) {
+	runCommands(sshClient, instanceId, instanceAddr, installTSCommands)
+}
+
+func installSNC(sshClient *ssh.Client, instanceId, instanceAddr string) {
+	runCommands(sshClient, instanceId, instanceAddr, installSNCCommands)
+}
+
+func installSCION(sshClient *ssh.Client, instanceId, instanceAddr string) {
+	runCommands(sshClient, instanceId, instanceAddr, installSCIONCommands)
+}
+
+func installGo(sshClient *ssh.Client, instanceId, instanceAddr string) {
+	runCommands(sshClient, instanceId, instanceAddr, installGoCommands)
 }
 
 func setupInstance(wg *sync.WaitGroup, instanceId, instanceAddr, sshIdentityFile string) {
@@ -165,6 +301,11 @@ func setupInstance(wg *sync.WaitGroup, instanceId, instanceAddr, sshIdentityFile
 	defer sshClient.Close()
 
 	runCommand(sshClient, instanceId, instanceAddr, "uname -a")
+	installGo(sshClient, instanceId, instanceAddr)
+	installSCION(sshClient, instanceId, instanceAddr)
+	installSNC(sshClient, instanceId, instanceAddr)
+	installTS(sshClient, instanceId, instanceAddr)
+	uploadTestnet(sshClient, instanceId, instanceAddr)
 }
 
 func setup(sshIdentityFile string) {
--- misc/ec2/go.mod
+++ misc/ec2/go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/aws/aws-sdk-go-v2 v1.20.3
 	github.com/aws/aws-sdk-go-v2/config v1.18.35
 	github.com/aws/aws-sdk-go-v2/service/ec2 v1.113.1
+	github.com/pkg/sftp v1.13.6
 	golang.org/x/crypto v0.12.0
 )
 
@@ -21,5 +22,6 @@ require (
 	github.com/aws/aws-sdk-go-v2/service/sts v1.21.4 // indirect
 	github.com/aws/smithy-go v1.14.2 // indirect
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	github.com/kr/fs v0.1.0 // indirect
 	golang.org/x/sys v0.11.0 // indirect
 )
