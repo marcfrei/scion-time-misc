8a388a183dfea83b405bbf2001681b97aa323eb2
Marc Frei <marc.frei@gmail.com>
2024-05-31 10:26:58 +0200

    Change test setup

--- misc/clockbench/clockbench.go
+++ misc/clockbench/clockbench.go
@@ -13,12 +13,12 @@ func main() {
 	t0 := clk.Now()
 	f0 := clk.Frequency()
 	log.Printf("%v: %v", t0.Sub(t0), f0)
-	clk.Adjust(10*time.Millisecond, 1*time.Second, f0)
+	// clk.Adjust(-10*time.Millisecond, 1*time.Minute, f0)
 	for {
 		t := clk.Now()
 		f := clk.Frequency()
 		log.Printf("%v: %v", t.Sub(t0), f)
-		if t.After(t0.Add(1 * time.Second)) {
+		if t.After(t0.Add(63 * time.Second)) {
 			break
 		}
 		clk.Sleep(100 * time.Millisecond)
